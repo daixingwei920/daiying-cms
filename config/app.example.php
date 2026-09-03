0974d20e1d5cedeacc77a1a5b64be2f3d9f53944
xingwei dai <xingweidai@Mac.sc.rr.com>
2026-09-03 17:35:19 -0400

    release: bump Daiying CMS to 1.2.20

--- config/app.example.php
+++ config/app.example.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'app' => [
         'name' => 'PHP CMS',
-        'version' => '1.2.19',
+        'version' => '1.2.20',
         'debug' => false,
         'mode' => 'NORMAL',
         'secure_cookies' => true,
